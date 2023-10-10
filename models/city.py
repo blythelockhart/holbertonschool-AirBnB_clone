ca1f051cee8c813507668ecd190078835664f39b
blythelockhart <blythe.lockhart@att.net>
2023-10-10 06:31:44 -0700

    coded User, State, Place, Amenity, City, and Review classes

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+""" City class. """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class that inherits from BaseModel. """
+    def __init__(self, *args, **kwargs):
+        """ Initialize City instance. """
+        super().__init__(*args, **kwargs)
+        self.state_id = ""
+        self.name = ""
